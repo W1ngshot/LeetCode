e71b69270e7a5ff1cf9bdafa9d33d689b1a587e4
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 15:44:56 +0300

    383 done

---
+++ 383. Ransom Note/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode._383._Ransom_Note;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var a = ransomNote
+            .ToCharArray()
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+        var b = magazine
+            .ToCharArray()
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, x => x.Count());
+
+        foreach (var key in a.Keys)
+        {
+            if (!b.TryGetValue(key, out var value) || value < a[key])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
