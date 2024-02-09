f12e6508bd02e641ffabc3058b6c1dfd9f24e873
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 18:15:09 +0300

    977 done

---
+++ 977. Squares of a Sorted Array/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._977._Squares_of_a_Sorted_Array;
+
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        var left = 0;
+        var right = nums.Length - 1;
+        var result = new int[nums.Length];
+        
+        var counter = right;
+        while (right >= left)
+        {
+            if (Math.Abs(nums[right]) > Math.Abs(nums[left]))
+            {
+                result[counter] = nums[right] * nums[right];
+                right -= 1;
+            }
+            else
+            {
+                result[counter] = nums[left] * nums[left];
+                left += 1;
+            }
+            counter--;
+        }
+        
+        return result;
+    }
+}
