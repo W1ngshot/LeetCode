ba4d85ece0326cb55447c0b62ecd51c982109ac1
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 22:19:19 +0300

    1089 done

---
+++ 1089. Duplicate Zeros/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._1089._Duplicate_Zeros;
+
+public class Solution
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        for (var i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] == 0)
+            {
+                for (var j = arr.Length - 1; j > i; j--)
+                {
+                    arr[j] = arr[j - 1];
+                }
+
+                i++;
+            }
+        }
+    }
+}
