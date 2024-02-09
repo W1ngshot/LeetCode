763a1dc1cf9017142c26a5fe530aea0bff65c92d
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 16:45:37 +0300

    485 done

---
+++ 485. Max Consecutive Ones/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._485._Max_Consecutive_Ones;
+
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        var max = 0;
+        var current = 0;
+        
+        foreach (var num in nums)
+        {
+            if (num == 1)
+            {
+                current++;
+            }
+            else
+            {
+                if (current > max)
+                    max = current;
+                current = 0;
+            }
+        }
+        
+        if (current > max)
+            max = current;
+        
+        return max;
+    }
+}
