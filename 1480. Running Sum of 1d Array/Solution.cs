d7c0bac15348405ca8f40a08b9e5fab4d6884615
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 03:53:24 +0300

    1480 done

---
+++ 1480. Running Sum of 1d Array/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._1480._Running_Sum_of_1d_Array;
+
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        var sum = 0;
+        var result = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+}
