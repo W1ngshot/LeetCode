f41f2a3a320c33b0f7db253ad844921a0842ba84
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 15:13:51 +0300

    1342 done

---
+++ 1342. Number of Steps to Reduce a Number to Zero/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._1342._Number_of_Steps_to_Reduce_a_Number_to_Zero;
+
+public class Solution {
+    public int NumberOfSteps(int num) {
+        var count = 0;
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
+    }
+}
