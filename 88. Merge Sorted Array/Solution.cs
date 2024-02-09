573d2e352694bf605ee53c2b16abbc1f77fbb55f
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 22:40:53 +0300

    88 done

---
+++ 88. Merge Sorted Array/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._88._Merge_Sorted_Array;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        var i = m - 1;
+        var j = n - 1;
+        var res = m + n - 1;
+
+        while (j >= 0)
+        {
+            if (i >= 0 && nums1[i] > nums2[j])
+            {
+                nums1[res--] = nums1[i--];
+            }
+            else
+            {
+                nums1[res--] = nums2[j--];
+            }
+        }
+    }
+}
