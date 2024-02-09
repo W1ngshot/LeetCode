1f43e7d1703687084b5f4cb1a2c69c31924df649
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 15:09:45 +0300

    412 done

---
+++ 412. Fizz Buzz/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode._412._Fizz_Buzz;
+
+public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        var result = new List<string>();
+        for (var x = 1; x <= n; x++)
+        {
+            result.Add(x % 15 == 0 ? "FizzBuzz" :
+                x % 3 == 0 ? "Fizz" :
+                x % 5 == 0 ? "Buzz" : x.ToString());
+        }
+        return result;
+    }
+}
