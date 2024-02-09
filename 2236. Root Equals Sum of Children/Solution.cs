e9db937c16672bdb11556b8e22b509db95b977c6
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 03:43:13 +0300

    2236 done

---
+++ 2236. Root Equals Sum of Children/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._2236._Root_Equals_Sum_of_Children;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool CheckTree(TreeNode root) =>
+        root.val == root.left.val + root.right.val;
+}
