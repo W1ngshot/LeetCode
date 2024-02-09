491145e4b7017a7577ef8be3765bd37775ccd4fd
W1ngshot <kirill-samsonov@bk.ru>
2024-02-09 04:07:45 +0300

    1672 done

---
+++ 1672. Richest Customer Wealth/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._1672._Richest_Customer_Wealth;
+
+public class Solution {
+    public int MaximumWealth(int[][] accounts) {
+        var max = 0;
+        for (var i = 0; i < accounts.Length; i++)
+        {
+            var sum = 0;
+            for (var j = 0; j < accounts[i].Length; j++)
+            {
+                sum += accounts[i][j];
+            }
+
+            if (sum > max)
+            {
+                max = sum;
+            }
+        }
+
+        return max;
+    }
+}
