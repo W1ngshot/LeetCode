cafd9873a96064e4df68d5f4152ee9d521a011ad
W1ngshot <kirill-samsonov@bk.ru>
2024-02-10 23:06:19 +0300

    27 done

---
+++ 27. Remove Element/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode._27._Remove_Element;
+
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        var left = 0;
+        var right = nums.Length - 1;
+        var result = nums.Length;
+
+        while (right >= left)
+        {
+            if (nums[right] == val)
+            {
+                result--;
+                right--;
+            }
+            else if (nums[left] == val)
+            {
+                nums[left++] = nums[right--];
+                result--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
