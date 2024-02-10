ad3a0be86c0c3e33c002678f24ccd592d6d25e2d
W1ngshot <kirill-samsonov@bk.ru>
2024-02-11 00:16:59 +0300

    26 done

---
+++ 26. Remove Duplicates from Sorted Array/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._26._Remove_Duplicates_from_Sorted_Array;
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+
+        var k = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[k] != nums[i])
+            {
+                nums[++k] = nums[i];
+            }
+        }
+        return k + 1;
+    }
+}
