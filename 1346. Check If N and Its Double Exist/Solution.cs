1b653c15feaa6e02ad83c3082ca461031b571bd5
W1ngshot <kirill-samsonov@bk.ru>
2024-02-11 23:37:31 +0300

    1346 done

---
+++ 1346. Check If N and Its Double Exist/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._1346._Check_If_N_and_Its_Double_Exist;
+
+public class Solution
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        var hashes = new HashSet<int>();
+        foreach (var el in arr)
+        {
+            if (el % 2 == 0 && hashes.TryGetValue(el / 2, out _) || hashes.TryGetValue(el * 2, out _))
+            {
+                return true;
+            }
+
+            hashes.Add(el);
+        }
+
+        return false;
+    }
+}
