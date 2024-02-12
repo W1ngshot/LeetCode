d658f103e0bc1b25011f8d6a894598600e3eb60c
W1ngshot <kirill-samsonov@bk.ru>
2024-02-12 10:56:40 +0300

    941 done

---
+++ 941. Valid Mountain Array/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode._941._Valid_Mountain_Array;
+
+public class Solution
+{
+    public bool ValidMountainArray(int[] arr)
+    {
+        if (arr.Length < 3 || arr[0] >= arr[1])
+        {
+            return false;
+        }
+
+        var inc = true;
+        var last = arr[0];
+        for (var i = 1; i < arr.Length; i++)
+        {
+            if (inc && arr[i] <= last)
+            {
+                inc = !inc;
+            }
+
+            if (!inc && arr[i] >= last)
+            {
+                return false;
+            }
+
+            last = arr[i];
+        }
+
+        return !inc;
+    }
+}
