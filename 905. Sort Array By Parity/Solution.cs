22bc357b647d99f22c1430c3eb5a052059963cc4
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 00:05:51 +0300

    905 done

---
+++ 905. Sort Array By Parity/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._905._Sort_Array_By_Parity;
+
+public class Solution
+{
+    public int[] SortArrayByParity(int[] nums)
+    {
+        var p = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                (nums[i], nums[p]) = (nums[p], nums[i]);
+                p++;
+            }
+        }
+
+        return nums;
+    }
+}
