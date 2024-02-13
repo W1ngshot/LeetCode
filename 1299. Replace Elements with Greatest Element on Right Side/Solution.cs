2b1a22935e0fdef40e8e1bfcb95726b8da95fc52
W1ngshot <kirill-samsonov@bk.ru>
2024-02-13 23:38:19 +0300

    1299 done

---
+++ 1299. Replace Elements with Greatest Element on Right Side/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._1299._Replace_Elements_with_Greatest_Element_on_Right_Side;
+
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        var max = arr[^1];
+        arr[^1] = -1;
+        for (var i = arr.Length - 2; i >= 0; i--)
+        {
+            var t = arr[i];
+            arr[i] = max;
+            if (t > max)
+            {
+                max = t;
+            }
+        }
+
+        return arr;
+    }
+}
