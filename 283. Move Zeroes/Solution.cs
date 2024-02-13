a9c9c368c315e3378f042876b1cf5534991b1abe
W1ngshot <kirill-samsonov@bk.ru>
2024-02-13 23:51:46 +0300

    283 done

---
+++ 283. Move Zeroes/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._283._Move_Zeroes;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var p1 = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (p1 != i && nums[i] != 0)
+            {
+                (nums[i], nums[p1]) = (nums[p1], nums[i]);
+                p1++;
+            }
+        }
+    }
+}
