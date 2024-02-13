4e2a4ccb188f0671a1b943a3313b4633d75b97aa
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 00:28:16 +0300

    922 done

---
+++ 922. Sort Array By Parity II/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._922._Sort_Array_By_Parity_II;
+
+public class Solution
+{
+    public int[] SortArrayByParityII(int[] nums)
+    {
+        var p = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (p % 2 == nums[p] % 2)
+            {
+                if (p != i)
+                {
+                    i--;
+                }
+
+                p++;
+            }
+            else if (p % 2 == nums[i] % 2)
+            {
+                (nums[i], nums[p]) = (nums[p], nums[i]);
+                p++;
+            }
+        }
+
+        return nums;
+    }
+}
