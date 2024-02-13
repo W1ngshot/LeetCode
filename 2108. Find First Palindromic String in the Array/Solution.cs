7b6937241a5102bdee4369aecabc6bf32e56e35c
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 00:41:38 +0300

    2108 done

---
+++ 2108. Find First Palindromic String in the Array/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._2108._Find_First_Palindromic_String_in_the_Array;
+
+public class Solution
+{
+    public string FirstPalindrome(string[] words)
+    {
+        foreach (var str in words)
+        {
+            int l = 0, r = str.Length - 1;
+            var isP = true;
+            while (l < r)
+            {
+                if (str[l++] != str[r--])
+                {
+                    isP = false;
+                    break;
+                }
+            }
+
+            if (isP)
+            {
+                return str;
+            }
+        }
+
+        return "";
+    }
+}
