6e36c527c41136969f52eddb5eb0e5ada62e4482
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 20:43:51 +0300

    2149 done

---
+++ 2149. Rearrange Array Elements by Sign/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._2149._Rearrange_Array_Elements_by_Sign;
+
+public class Solution
+{
+    public int[] RearrangeArray(int[] nums)
+    {
+        var res = new int[nums.Length];
+        int p = 0, n = 1;
+        foreach (var num in nums)
+        {
+            if (num > 0)
+            {
+                res[p] = num;
+                p += 2;
+            }
+            else
+            {
+                res[n] = num;
+                n += 2;
+            }
+        }
+
+        return res;
+    }
+}
