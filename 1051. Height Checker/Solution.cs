8937e57377a04c4e30e15e788b0478fae0759ee2
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 20:56:59 +0300

    1051 done

---
+++ 1051. Height Checker/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode._1051._Height_Checker;
+
+public class Solution
+{
+    public int HeightChecker(int[] heights)
+    {
+        return heights.OrderBy(x => x)
+            .Where((t, i) => heights[i] != t)
+            .Count();
+    }
+}
