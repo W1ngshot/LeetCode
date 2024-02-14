bc2c9a51d8be6aaa94d005a6fe3cf398009c8694
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 21:44:01 +0300

    448 done

---
+++ 448. Find All Numbers Disappeared in an Array/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._448._Find_All_Numbers_Disappeared_in_an_Array;
+
+public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var index = Math.Abs(nums[i] - 1);
+            nums[index] = -Math.Abs(nums[index]);
+        }
+
+        var res = new List<int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                res.Add(i + 1);
+            }
+        }
+
+        return res;
+    }
+}
