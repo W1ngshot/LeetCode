05450576ac3f1fc9a39a5a172712b79f8ba054f1
W1ngshot <kirill-samsonov@bk.ru>
2024-02-14 21:20:08 +0300

    414 done

---
+++ 414. Third Maximum Number/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode._414._Third_Maximum_Number;
+
+public class Solution
+{
+    public int ThirdMax(int[] nums)
+    {
+        (int max1, int? max2, int? max3) = (nums[0], null, null);
+        foreach (var num in nums)
+        {
+            if (num == max1 || num == max2 || num == max3)
+                continue;
+
+            if (num > max1)
+            {
+                (max1, max2, max3) = (num, max1, max2);
+            }
+            else if (max2 is null || num > max2)
+            {
+                (max2, max3) = (num, max2);
+            }
+            else if (max3 is null || num > max3)
+            {
+                max3 = num;
+            }
+        }
+
+        return max3 ?? max1;
+    }
+}
