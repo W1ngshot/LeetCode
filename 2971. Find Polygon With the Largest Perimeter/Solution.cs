83dafb593af1d1157ae94e3047c4febfd6ff7983
W1ngshot <kirill-samsonov@bk.ru>
2024-02-15 16:26:15 +0300

    2971 done

---
+++ 2971. Find Polygon With the Largest Perimeter/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._2971._Find_Polygon_With_the_Largest_Perimeter;
+
+public class Solution
+{
+    public long LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        var prSum = new long[nums.Length];
+        long sum = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            prSum[i] = sum;
+        }
+
+        for (var i = nums.Length - 1; i > 1; i--)
+        {
+            if (nums[i] < prSum[i - 1]) return prSum[i];
+        }
+
+        return -1;
+    }
+}
