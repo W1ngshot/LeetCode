c7f58f5154ea89fff01f62684cc4fac0f731c482
W1ngshot <kirill-samsonov@bk.ru>
2024-02-16 12:33:05 +0300

    1481 done

---
+++ 1481. Least Number of Unique Integers after K Removals/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._1481._Least_Number_of_Unique_Integers_after_K_Removals;
+
+public class Solution
+{
+    public int FindLeastNumOfUniqueInts(int[] arr, int k)
+    {
+        var dict = arr.GroupBy(x => x)
+            .OrderBy(x => x.Count())
+            .Select(x => x.Count())
+            .ToList();
+
+        var count = dict.Count;
+        foreach (var val in dict)
+        {
+            if (val > k) return count;
+            k -= val;
+            count--;
+        }
+
+        return count;
+    }
+}
