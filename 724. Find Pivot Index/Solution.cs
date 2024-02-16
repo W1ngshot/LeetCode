5907f64afcb88a0440e8700a996cb654b2f9ba45
W1ngshot <kirill-samsonov@bk.ru>
2024-02-16 13:45:01 +0300

    724 done

---
+++ 724. Find Pivot Index/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._724._Find_Pivot_Index;
+
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        var sum = nums.Sum();
+        var leftSum = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == sum - leftSum - nums[i])
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
