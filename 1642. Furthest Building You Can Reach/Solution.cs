737be8c274ecec0a1abe944397f4e12c2c3da1f5
W1ngshot <kirill-samsonov@bk.ru>
2024-02-17 15:21:02 +0300

    1642 done

---
+++ 1642. Furthest Building You Can Reach/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._1642._Furthest_Building_You_Can_Reach;
+
+public class Solution
+{
+    public int FurthestBuilding(int[] heights, int bricks, int ladders)
+    {
+        var queue = new PriorityQueue<int, int>();
+        for (var i = 0; i < heights.Length - 1; i++)
+        {
+            var dif = heights[i + 1] - heights[i];
+            if (dif <= 0)
+                continue;
+
+            queue.Enqueue(dif, dif);
+
+            if (queue.Count > ladders)
+                bricks -= queue.Dequeue();
+
+            if (bricks < 0)
+                return i;
+        }
+
+        return heights.Length - 1;
+    }
+}
