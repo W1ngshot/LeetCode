4767f0ddbb2df548a95af4166224c90c59b91891
W1ngshot <kirill-samsonov@bk.ru>
2024-02-20 03:13:13 +0300

    268 done

---
+++ 268. Missing Number/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._268._Missing_Number;
+
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+    }
+}
