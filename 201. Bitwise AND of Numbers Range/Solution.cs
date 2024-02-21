bdadf08fe9581a1141a18f5aaec8776f4a8f04a0
W1ngshot <kirill-samsonov@bk.ru>
2024-02-22 00:15:57 +0300

    201 done

---
+++ 201. Bitwise AND of Numbers Range/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._201._Bitwise_AND_of_Numbers_Range;
+
+public class Solution
+{
+    public int RangeBitwiseAnd(int left, int right)
+    {
+        while (left < right)
+        {
+            right &= (right - 1);
+        }
+
+        return right;
+    }
+}
