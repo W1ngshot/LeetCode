9801d0b17252794a15912635fe3795ac21a4f95b
W1ngshot <kirill-samsonov@bk.ru>
2024-02-24 01:44:28 +0300

    1 done

---
+++ 1. Two Sum/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1._Two_Sum;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var exists = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (exists.TryGetValue(target - nums[i], out var value))
+                return [i, value];
+            exists[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
