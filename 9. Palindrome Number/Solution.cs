8af939209b2fc80198d9466506dbbb3421b6b5f4
W1ngshot <kirill-samsonov@bk.ru>
2024-02-24 13:57:40 +0300

    9 done

---
+++ 9. Palindrome Number/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._9._Palindrome_Number;
+
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        var s = x.ToString();
+        int l = 0, r = s.Length - 1;
+        while (l < r)
+        {
+            if (s[l++] != s[r--])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
