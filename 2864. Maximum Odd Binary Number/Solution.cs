ff93f66be0eea7e6c1aa9879b92347ce285401f5
W1ngshot <kirill-samsonov@bk.ru>
2024-03-03 14:15:24 +0300

    2864 done

---
+++ 2864. Maximum Odd Binary Number/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace LeetCode._2864._Maximum_Odd_Binary_Number;
+
+public class Solution
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        var counter = s.Count(x => x == '1');
+        var sb = new StringBuilder();
+        for (var i = 0; i < counter - 1; i++)
+        {
+            sb.Append('1');
+        }
+
+        for (var i = 0; i < s.Length - counter; i++)
+        {
+            sb.Append('0');
+        }
+
+        sb.Append('1');
+        return sb.ToString();
+    }
+}
