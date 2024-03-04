0d9f4fff355b9ea184aeafd28706a322119eea4a
W1ngshot <kirill-samsonov@bk.ru>
2024-03-04 20:47:08 +0300

    948 done

---
+++ 948. Bag of Tokens/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._948._Bag_of_Tokens;
+
+public class Solution
+{
+    public int BagOfTokensScore(int[] tokens, int power)
+    {
+        Array.Sort(tokens);
+        var score = 0;
+        int l = 0, r = tokens.Length - 1;
+        while (l <= r)
+        {
+            if (power >= tokens[l])
+            {
+                power -= tokens[l++];
+                score++;
+            }
+            else if (tokens[r] > tokens[l] && score > 0)
+            {
+                power += tokens[r--];
+                score--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return score;
+    }
+}
