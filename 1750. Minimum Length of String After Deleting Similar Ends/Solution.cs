aa025bd4a0d1edec456fc4121e7e5262a540c573
W1ngshot <kirill-samsonov@bk.ru>
2024-03-05 14:38:50 +0300

    1750 done

---
+++ 1750. Minimum Length of String After Deleting Similar Ends/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._1750._Minimum_Length_of_String_After_Deleting_Similar_Ends;
+
+public class Solution
+{
+    public int MinimumLength(string s)
+    {
+        int l = 0, r = s.Length - 1;
+        while (l < r && s[l] == s[r])
+        {
+            var ch = s[l];
+            while (l < r && s[l] == ch)
+                l++;
+            while (l < r && s[r] == ch)
+                r--;
+        }
+
+        return s.Length - l * 2;
+    }
+}
