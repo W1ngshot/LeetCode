0aaa49a98d68b3e5d63e47ffe2a5920bdb366691
W1ngshot <kirill-samsonov@bk.ru>
2024-03-06 19:41:49 +0300

    141 done

---
+++ 141. Linked List Cycle/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._141._Linked_List_Cycle;
+
+//Definition for singly-linked list.
+public class ListNode(int x, ListNode? next)
+{
+    public int val = x;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var hashSet = new HashSet<ListNode>();
+        var current = head;
+        while (current is not null)
+        {
+            if (!hashSet.Add(current))
+                return true;
+            current = current.next;
+        }
+
+        return false;
+    }
+}
