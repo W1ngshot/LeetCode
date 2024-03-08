c1dc2a96da9b1e7c41ccb18e5c0cc1f2d000b206
W1ngshot <kirill-samsonov@bk.ru>
2024-03-08 18:32:41 +0300

    3005 done

---
+++ 3005. Count Elements With Maximum Frequency/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._3005._Count_Elements_With_Maximum_Frequency;
+
+public class Solution
+{
+    public int MaxFrequencyElements(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (!dict.TryAdd(num, 1))
+                dict[num]++;
+        }
+
+        int max = 0, count = 0;
+        foreach (var value in dict.Values)
+        {
+            if (value > max)
+            {
+                max = value;
+                count = 1;
+            }
+            else if (value == max)
+                count++;
+        }
+
+        return max * count;
+    }
+}
