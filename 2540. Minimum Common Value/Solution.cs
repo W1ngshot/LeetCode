f51fda9d49952ed9032530287f44e615080b10d9
W1ngshot <kirill-samsonov@bk.ru>
2024-03-09 16:35:16 +0300

    2540 done

---
+++ 2540. Minimum Common Value/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode._2540._Minimum_Common_Value;
+
+public class Solution
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        int first = 0, second = 0;
+        while (first < nums1.Length && second < nums2.Length)
+        {
+            if (nums1[first] == nums2[second])
+                return nums1[first];
+            
+            if (nums1[first] < nums2[second])
+                first++;
+            else
+                second++;
+        }
+
+        return -1;
+    }
+}
