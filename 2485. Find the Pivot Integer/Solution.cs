b24ca166176e28131accf49161c49f1e663ac8bf
W1ngshot <kirill-samsonov@bk.ru>
2024-03-13 18:27:46 +0300

    2485 done

---
+++ 2485. Find the Pivot Integer/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._2485._Find_the_Pivot_Integer;
+
+public class Solution
+{
+    public int PivotInteger(int n)
+    {
+        var sum = (1 + n) * n / 2;
+        var newSum = 0;
+        for (var i = n; i > 0; i--)
+        {
+            if (newSum == (sum - i) / 2d)
+                return i;
+            if (newSum > (sum - i) / 2d)
+                return -1;
+            newSum += i;
+        }
+        return -1;
+    }
+}
