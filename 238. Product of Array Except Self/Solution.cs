ccab037c541c2fb0a92330ffadc5977e430faba4
W1ngshot <kirill-samsonov@bk.ru>
2024-03-15 13:41:37 +0300

    238 done

---
+++ 238. Product of Array Except Self/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._238._Product_of_Array_Except_Self;
+
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        var result = new int[nums.Length];
+        for (var i = 0; i < nums.Length; i++)
+            result[i] = 1;
+
+        int l = 1, r = 1;
+        for (int i = 1, j = nums.Length - 2; i < nums.Length; i++, j--)
+        {
+            l *= nums[i];
+            r *= nums[j];
+            result[i] *= l;
+            result[j] *= r;
+        }
+
+        return result;
+    }
+}
