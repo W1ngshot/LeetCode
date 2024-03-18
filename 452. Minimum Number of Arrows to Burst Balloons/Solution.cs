790314ff8175a99f5a7092fc3af2445e6ec2ee59
W1ngshot <kirill-samsonov@bk.ru>
2024-03-18 14:54:42 +0300

    452 done

---
+++ 452. Minimum Number of Arrows to Burst Balloons/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._452._Minimum_Number_of_Arrows_to_Burst_Balloons;
+
+public class Solution
+{
+    public int FindMinArrowShots(int[][] points)
+    {
+        points = points.OrderBy(x => x[1]).ToArray();
+        var counter = 1;
+        var minPoint = points[0];
+
+        for (var i = 1; i < points.Length; i++)
+        {
+            if (points[i][0] > minPoint[1])
+            {
+                counter++;
+                minPoint = points[i];
+            }
+        }
+
+        return counter;
+    }
+}
