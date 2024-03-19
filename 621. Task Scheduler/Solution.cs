3f1757f334ee8e46ca3fc8ba1f67809d6d36321c
W1ngshot <kirill-samsonov@bk.ru>
2024-03-19 23:52:53 +0300

    621 done

---
+++ 621. Task Scheduler/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._621._Task_Scheduler;
+
+public class Solution
+{
+    public int LeastInterval(char[] tasks, int n)
+    {
+        var frequencies = tasks
+            .GroupBy(x => x)
+            .OrderByDescending(x => x.Count())
+            .ToDictionary(x => x.Key, x => x.Count());
+
+        var maxFreq = frequencies.First().Value - 1;
+        var idle = maxFreq * n;
+
+        foreach (var freq in frequencies.Values.Skip(1))
+        {
+            idle -= Math.Min(maxFreq, freq);
+        }
+
+        return tasks.Length + Math.Max(0, idle);
+    }
+}
