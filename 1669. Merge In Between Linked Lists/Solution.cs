acfaf22481e2986942bd551c20a5ae487e648df6
W1ngshot <kirill-samsonov@bk.ru>
2024-03-20 21:11:07 +0300

    1669 done

---
+++ 1669. Merge In Between Linked Lists/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode._1669._Merge_In_Between_Linked_Lists;
+
+//Definition for singly-linked list.
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public ListNode MergeInBetween(ListNode list1, int a, int b, ListNode list2)
+    {
+        var result = new ListNode
+        {
+            next = list1
+        };
+        var first = result;
+
+        for (var i = 0; i < a; i++)
+        {
+            first = first!.next;
+        }
+
+        var second = first!.next;
+        for (var i = a; i <= b; i++)
+        {
+            second = second!.next;
+        }
+
+        first!.next = list2;
+
+        while (list2.next != null)
+        {
+            list2 = list2.next;
+        }
+
+        list2.next = second;
+
+        return result.next;
+    }
+}
