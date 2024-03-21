66a789f7036288c660bb973c22d9e751a5ab798c
W1ngshot <kirill-samsonov@bk.ru>
2024-03-21 12:01:07 +0300

    206 done

---
+++ 206. Reverse Linked List/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._206._Reverse_Linked_List;
+
+//Definition for singly-linked list.
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        if (head is null || head.next is null)
+            return head;
+        var current = head.next;
+        var prev = head;
+        prev.next = null;
+
+        while (current is not null)
+        {
+            var next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
