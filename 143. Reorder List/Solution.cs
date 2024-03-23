e65570bc27bd8e13e787a80cd9e1a19ad74dabe7
W1ngshot <kirill-samsonov@bk.ru>
2024-03-24 01:50:06 +0300

    143 done

---
+++ 143. Reorder List/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode._143._Reorder_List;
+
+// Definition for singly-linked list.
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public void ReorderList(ListNode head)
+    {
+        ListNode slow = head, fast = head.next;
+
+        while (fast?.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode left = head, right = slow.next;
+        slow.next = null;
+
+        ListNode prev = null, curr = right;
+        while (curr is not null)
+        {
+            var next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        right = prev;
+
+        while (right is not null)
+        {
+            ListNode l = left.next, r = right.next;
+            left.next = right;
+            right.next = l;
+
+            left = l;
+            right = r;
+        }
+    }
+}
