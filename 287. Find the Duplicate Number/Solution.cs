a374449fee0b1dcc1fe3803ed0f2ac6a5e7dbaad
W1ngshot <kirill-samsonov@bk.ru>
2024-03-24 12:59:42 +0300

    287 done

---
+++ 287. Find the Duplicate Number/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode._287._Find_the_Duplicate_Number;
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        return nums.FirstOrDefault(num => !dict.TryAdd(num, 1));
+    }
+}
