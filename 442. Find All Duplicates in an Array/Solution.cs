1de76bb5fc843fc9aa6bc8f73aa052a5ed2bb735
W1ngshot <kirill-samsonov@bk.ru>
2024-03-25 15:07:39 +0300

    442 done

---
+++ 442. Find All Duplicates in an Array/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode._442._Find_All_Duplicates_in_an_Array;
+
+public class Solution
+{
+    public IList<int> FindDuplicates(int[] nums)
+    {
+        var result = new List<int>();
+
+        foreach (var num in nums)
+        {
+            var index = Math.Abs(num) - 1;
+            if (nums[index] < 0)
+                result.Add(Math.Abs(num));
+            nums[index] = -nums[index];
+        }
+
+        return result;
+    }
+}
