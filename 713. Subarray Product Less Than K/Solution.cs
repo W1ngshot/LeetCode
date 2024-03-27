4b0310b5d9544a97f09ca2ae57ed54ad95fb89d1
W1ngshot <kirill-samsonov@bk.ru>
2024-03-27 03:36:02 +0300

    713 done

---
+++ 713. Subarray Product Less Than K/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._713._Subarray_Product_Less_Than_K;
+
+public class Solution
+{
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        var result = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var j = i;
+            var current = 1;
+            while (j >= 0 && current * nums[j] < k)
+            {
+                current *= nums[j];
+                j--;
+            }
+
+            result += i - j;
+        }
+
+        return result;
+    }
+}
