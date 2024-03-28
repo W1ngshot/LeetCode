d32cad149ebf16c1a4c325ec918c8320030b0e65
W1ngshot <kirill-samsonov@bk.ru>
2024-03-29 00:52:55 +0300

    2958 done

---
+++ 2958. Length of Longest Subarray With at Most K Frequency/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode._2958._Length_of_Longest_Subarray_With_at_Most_K_Frequency;
+
+public class Solution
+{
+    public int MaxSubarrayLength(int[] nums, int k)
+    {
+        var max = 0;
+        var dict = new Dictionary<int, int>();
+        var start = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!dict.TryGetValue(nums[i], out var value))
+                dict[nums[i]] = 1;
+            else
+                dict[nums[i]] = ++value;
+            
+            if (value <= k)
+            {
+                if (i - start + 1 > max)
+                    max = i - start + 1;
+            }
+            else
+            {
+                while (dict[nums[i]] > k && start <= i)
+                {
+                    dict[nums[start]]--;
+                    start++;
+                }
+            }
+        }
+
+        return max;
+    }
+}
