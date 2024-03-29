2239192930aa281237600b74ede22fdff0a435db
W1ngshot <kirill-samsonov@bk.ru>
2024-03-29 14:57:35 +0300

    2962 done

---
+++ 2962. Count Subarrays Where Max Element Appears at Least K Times/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._2962._Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times;
+
+public class Solution
+{
+    public long CountSubarrays(int[] nums, int k)
+    {
+        long counter = 0;
+        var dict = new Dictionary<int, int>();
+        int max = nums.Max(), left = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!dict.TryGetValue(nums[i], out var value))
+                dict[nums[i]] = 1;
+            else
+                dict[nums[i]] = ++value;
+
+            while (dict[nums[i]] >= k && nums[i] == max)
+            {
+                counter += nums.Length - i;
+                dict[nums[left++]]--;
+            }
+        }
+
+        return counter;
+    }
+}
