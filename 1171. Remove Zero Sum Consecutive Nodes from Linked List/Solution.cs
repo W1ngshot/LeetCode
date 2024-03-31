cb8a094e03883acca47153e9a453cca419d5bb0e
W1ngshot <kirill-samsonov@bk.ru>
2024-03-31 14:57:16 +0300

    1171 done

---
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode._1171._Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List;
+
+//Definition for singly-linked list.
+
+public class ListNode(int val = 0, ListNode? next = null)
+{
+    public int val = val;
+    public ListNode? next = next;
+}
+
+public class Solution
+{
+    public ListNode RemoveZeroSumSublists(ListNode head)
+    {
+        var dict = new Dictionary<int, ListNode>();
+        var sum = 0;
+        var start = new ListNode(0, head);
+
+        var current = start;
+        while (current is not null)
+        {
+            sum += current.val;
+            dict[sum] = current;
+            current = current.next;
+        }
+
+        current = start;
+        sum = 0;
+
+        while (current is not null)
+        {
+            sum += current.val;
+            current.next = dict[sum].next;
+            current = current.next;
+        }
+
+        return start.next!;
+    }
+}
