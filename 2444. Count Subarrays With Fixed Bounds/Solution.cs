320d4d945b3d82950c5f351d1c5db42b9d5761be
W1ngshot <kirill-samsonov@bk.ru>
2024-03-31 14:17:35 +0300

    2444 done

---
+++ 2444. Count Subarrays With Fixed Bounds/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._2444._Count_Subarrays_With_Fixed_Bounds;
+
+public class Solution
+{
+    public long CountSubarrays(int[] nums, int minK, int maxK)
+    {
+        int minIndex = -1, maxIndex = -1, start = 0;
+        var counter = 0L;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] >= minK && nums[i] <= maxK)
+            {
+                maxIndex = nums[i] == maxK ? i : maxIndex;
+                minIndex = nums[i] == minK ? i : minIndex;
+                counter += Math.Max(0, Math.Min(maxIndex, minIndex) - start + 1);
+            }
+            else
+            {
+                maxIndex = -1;
+                minIndex = -1;
+                start = i + 1;
+            }
+        }
+
+        return counter;
+    }
+}
