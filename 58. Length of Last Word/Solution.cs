bb08a72c1563f86ba1c8f011f0a070c44a60b9ae
W1ngshot <kirill-samsonov@bk.ru>
2024-04-02 00:19:15 +0300

    58 done

---
+++ 58. Length of Last Word/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._58._Length_of_Last_Word;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        return s.TrimEnd().Split(' ')[^1].Length;
+    }
+}
