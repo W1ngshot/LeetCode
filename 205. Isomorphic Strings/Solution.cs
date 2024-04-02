c79aa72f2d2497fff9478bfe6c280e67559227e7
W1ngshot <kirill-samsonov@bk.ru>
2024-04-02 12:16:49 +0300

    205 done

---
+++ 205. Isomorphic Strings/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._205._Isomorphic_Strings;
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        var dictS = new Dictionary<char, char>();
+        var dictT = new Dictionary<char, char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (dictS.TryGetValue(s[i], out var symbol))
+            {
+                if (symbol != t[i])
+                    return false;
+            }
+            else if (dictT.ContainsKey(t[i]))
+            {
+                return false;
+            }
+            else
+            {
+                dictS[s[i]] = t[i];
+                dictT[t[i]] = s[i];
+            }
+        }
+
+        return true;
+    }
+}
