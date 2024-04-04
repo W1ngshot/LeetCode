74278e7e3e43dfaf635dcaf95f3e7474e4fd2c73
W1ngshot <kirill-samsonov@bk.ru>
2024-04-04 21:27:31 +0300

    1614 done

---
+++ 1614. Maximum Nesting Depth of the Parentheses/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._1614._Maximum_Nesting_Depth_of_the_Parentheses;
+
+public class Solution
+{
+    public int MaxDepth(string s)
+    {
+        var stack = new Stack<char>();
+        var max = 0;
+        foreach (var ch in s)
+        {
+            if (ch == '(')
+            {
+                stack.Push('(');
+                if (stack.Count > max)
+                    max = stack.Count;
+            }
+
+            if (ch == ')')
+                stack.Pop();
+        }
+
+        return max;
+    }
+}
