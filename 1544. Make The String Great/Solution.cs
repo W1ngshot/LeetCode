43d9ac7050a394187334fda29e0f441b21b0a623
W1ngshot <kirill-samsonov@bk.ru>
2024-04-05 17:33:11 +0300

    1544 done

---
+++ 1544. Make The String Great/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCode._1544._Make_The_String_Great;
+
+public class Solution
+{
+    public string MakeGood(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var ch in s)
+        {
+            if (stack.Count > 0 &&
+                (char.IsLower(ch) && char.ToUpper(ch) == stack.Peek()
+                 || char.IsUpper(ch) && char.ToLower(ch) == stack.Peek()))
+            {
+                stack.Pop();
+                continue;
+            }
+            stack.Push(ch);
+        }
+
+        var stringArray = stack.ToArray();
+        var sb = new StringBuilder();
+        for (var i = stringArray.Length - 1; i >= 0; i--)
+        {
+            sb.Append(stringArray[i]);
+        }
+
+        return sb.ToString();
+    }
+}
