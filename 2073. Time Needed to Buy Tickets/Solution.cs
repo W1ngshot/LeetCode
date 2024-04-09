9cdcf825e28a671057c8fd3956386282f7a323c6
W1ngshot <kirill-samsonov@bk.ru>
2024-04-09 16:04:28 +0300

    2073 done

---
+++ 2073. Time Needed to Buy Tickets/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode._2073._Time_Needed_to_Buy_Tickets;
+
+public class Solution
+{
+    public int TimeRequiredToBuy(int[] tickets, int k)
+    {
+        var counter = 0;
+        for (var i = 0; i < tickets.Length; i++)
+        {
+            if (i > k)
+                counter += tickets[i] < tickets[k] ? tickets[i] : tickets[k] - 1;
+            else
+                counter += Math.Min(tickets[i], tickets[k]);
+        }
+
+        return counter;
+    }
+}
