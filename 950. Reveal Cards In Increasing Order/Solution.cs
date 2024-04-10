43f236d23e92c67a85288c7fba8196c9953b59f7
W1ngshot <kirill-samsonov@bk.ru>
2024-04-10 16:55:02 +0300

    950 done

---
+++ 950. Reveal Cards In Increasing Order/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode._950._Reveal_Cards_In_Increasing_Order;
+
+public class Solution
+{
+    public int[] DeckRevealedIncreasing(int[] deck)
+    {
+        Array.Sort(deck);
+        var result = new int[deck.Length];
+        var queue = new Queue<int>(Enumerable.Range(0, deck.Length));
+
+        foreach (var card in deck)
+        {
+            result[queue.Dequeue()] = card;
+            if (queue.Count > 0)
+                queue.Enqueue(queue.Dequeue());
+        }
+
+        return result;
+    }
+}
