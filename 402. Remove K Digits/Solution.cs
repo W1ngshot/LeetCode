2e68d35e0ae22745bd1205e283c5d25ae2a5602e
W1ngshot <kirill-samsonov@bk.ru>
2024-04-11 13:30:51 +0300

    402 done

---
+++ 402. Remove K Digits/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LeetCode._402._Remove_K_Digits;
+
+public class Solution
+{
+    public string RemoveKdigits(string num, int k)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var ch in num)
+        {
+            while (k > 0 && sb.Length > 0 && sb[^1] > ch)
+            {
+                sb.Remove(sb.Length - 1, 1);
+                k--;
+            }
+            sb.Append(ch);
+        }
+
+        while (k > 0)
+        {
+            sb.Remove(sb.Length - 1, 1);
+            k--;
+        }
+
+        while (sb.Length > 1 && sb[0] == '0')
+        {
+            sb.Remove(0, 1);
+        }
+
+        return sb.Length == 0 ? "0" : sb.ToString();
+    }
+}
