efe55ce32a6bfaf5dbae7e288a5471e4c61f521e
W1ngshot <kirill-samsonov@bk.ru>
2024-04-25 01:42:03 +0300

    1137 done

---
+++ 1137. N-th Tribonacci Number/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._1137._N_th_Tribonacci_Number;
+
+public class Solution
+{
+    public int Tribonacci(int n)
+    {
+        int f = 0, s = 1, t = 1;
+
+        switch (n)
+        {
+            case 0:
+                return 0;
+            case 1 or 2:
+                return 1;
+            default:
+                for (var i = 3; i <= n; i++)
+                {
+                    (f, s, t) = (s, t, f + s + t);
+                }
+
+                return t;
+        }
+    }
+}
