9e3523f40fd695486d8e301ae0b1c216781ae4f6
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-16 19:43:20 +0300

    80 done

---
+++ 80. Remove Duplicates from Sorted Array II/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode._80._Remove_Duplicates_from_Sorted_Array_II;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var pointer = 0;
+        var currentNum = 0;
+        var currentCounter = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (currentNum == nums[i])
+            {
+                currentCounter++;
+                
+                if (currentCounter > 2)
+                    continue;
+            }
+            else
+            {
+                currentNum = nums[i];
+                currentCounter = 1;
+            }
+            nums[pointer] = nums[i];
+            pointer++;
+        }
+
+        return pointer;
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,6 @@
+﻿using LeetCode._80._Remove_Duplicates_from_Sorted_Array_II;
+
+var arr = new[] {1, 1};
+var k = new Solution().RemoveDuplicates(arr);
+Console.WriteLine(k);
+Console.WriteLine(string.Join(" ", arr));
