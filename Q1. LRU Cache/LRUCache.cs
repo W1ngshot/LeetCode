3ff20fdcd3067ed78dbf1be483251bdf1871c9c3
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-20 20:23:31 +0300

    q1 cache system design done

---
+++ Q1. LRU Cache/LRUCache.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Q1._LRU_Cache;
+
+public class LRUCache(int capacity)
+{
+    private readonly Dictionary<int, LinkedListNode<(int Key, int Value)>> _cache = new();
+    private readonly LinkedList<(int Key, int Value)> _list = [];
+
+    public int Get(int key) {
+        if (!_cache.TryGetValue(key, out var node)) 
+            return -1;
+        
+        
+        _list.Remove(node);
+        _list.AddLast(node);
+
+        return node.Value.Value;
+    }
+    
+    public void Put(int key, int value) {
+        
+        if (!_cache.TryGetValue(key, out var node))
+        {
+            if (_list.Count == capacity)
+            {
+                _cache.Remove(_list.First!.Value.Key);
+                _list.RemoveFirst();
+            }
+
+            var addNode = new LinkedListNode<(int Key, int Value)>((key, value));
+            _cache.Add(key, addNode);
+            _list.AddLast(addNode);
+            return;
+        }
+
+        _list.Remove(node);
+        _cache.Remove(key);
+
+        var newNode = new LinkedListNode<(int Key, int Value)>((key, value));
+        _list.AddLast(newNode);
+        _cache.Add(key, newNode);
+    }
+}
+
+/*
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.Get(key);
+ * obj.Put(key,value);
+ */
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,17 @@
-﻿using LeetCode._169._Majority_Element;
+﻿using LeetCode.Q1._LRU_Cache;
 
-var arr = new[] {2,2,1,1,1,2,2};
-var k = new Solution().MajorityElement(arr);
-Console.WriteLine(k);
+var cache = new LRUCache(2);
+cache.Put(1, 1);
+cache.Put(2, 2);
+var ans1 = cache.Get(1);
+Console.WriteLine(ans1);
+cache.Put(3, 3);
+var ans2 = cache.Get(2);
+Console.WriteLine(ans2);
+cache.Put(4, 4);
+var ans3 = cache.Get(1);
+Console.WriteLine(ans3);
+var ans4 = cache.Get(3);
+Console.WriteLine(ans4);
+var ans5 = cache.Get(4);
+Console.WriteLine(ans5);
