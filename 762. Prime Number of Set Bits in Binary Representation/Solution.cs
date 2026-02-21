324c45ef296a994da2e3b9951f4b45ad63fe023a
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-21 23:41:04 +0300

    762 done

---
+++ 762. Prime Number of Set Bits in Binary Representation/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace LeetCode._762._Prime_Number_of_Set_Bits_in_Binary_Representation;
+
+public class Solution
+{
+    public int CountPrimeSetBits(int left, int right)
+    {
+        var primes = new HashSet<int> { 2, 3, 5, 7, 11, 13, 17, 19 };
+        var count = 0;
+
+        for (var i = left; i <= right; i++)
+        {
+            var bitsCount = BitOperations.PopCount((uint)i);
+            
+            if (primes.Contains(bitsCount))
+                count++;
+        }
+        
+        return count;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,19 +1,7 @@
-﻿using LeetCode.Q2._LFU_Cache;
+﻿using LeetCode._762._Prime_Number_of_Set_Bits_in_Binary_Representation;
 
-var cache = new LFUCache(2);
-cache.Put(1, 1);
-cache.Put(2, 2);
-var ans1 = cache.Get(1);
-Console.WriteLine(ans1);
-cache.Put(3, 3);
-var ans2 = cache.Get(2);
-Console.WriteLine(ans2);
-var ans3 = cache.Get(3);
-Console.WriteLine(ans3);
-cache.Put(4, 4);
-var ans4 = cache.Get(1);
-Console.WriteLine(ans4);
-var ans5 = cache.Get(3);
-Console.WriteLine(ans5);
-var ans6 = cache.Get(4);
-Console.WriteLine(ans6);
+var k = new Solution();
+const int left = 6;
+const int right = 10;
+var  result = k.CountPrimeSetBits(left, right);
+Console.WriteLine(result);
