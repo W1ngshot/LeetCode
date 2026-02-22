4c4a28ef809150dd217d7b5ac1354790809c9a16
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-22 18:03:37 +0300

    868 done

---
+++ 868. Binary Gap/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._868._Binary_Gap;
+
+public class Solution
+{
+    public int BinaryGap(int n)
+    {
+        var binary = Convert.ToString(n, 2);
+        var count = 0;
+        var prevPtr = 0;
+
+        for (var i = 1; i < binary.Length; i++)
+        {
+            if (binary[i] != '1')
+                continue;
+
+            count = i - prevPtr > count ? i - prevPtr : count;
+            prevPtr = i;
+        }
+
+        return count;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,7 @@
-﻿using LeetCode._189._Rotate_Array;
+﻿using LeetCode._868._Binary_Gap;
 
 var solution = new Solution();
 
-var arr = new[] {1,2,3,4,5,6};
-const int k = 2;
-solution.Rotate(arr, k);
-Console.WriteLine(string.Join(" ", arr));
+const int k = 9;
+var ans = solution.BinaryGap(k);
+Console.WriteLine(ans);
