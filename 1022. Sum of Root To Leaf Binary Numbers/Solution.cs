dd2da862fb46b1231a53892ea0d7101717700a32
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-24 05:43:55 +0300

    1022 done

---
+++ 1022. Sum of Root To Leaf Binary Numbers/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode._1022._Sum_of_Root_To_Leaf_Binary_Numbers;
+
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
+
+public class Solution
+{
+    public int SumRootToLeaf(TreeNode root)
+    {
+        var sum = 0;
+
+        var stack = new Stack<(TreeNode Node, int Value)>();
+        stack.Push((root, root.val));
+
+        while (stack.Count > 0)
+        {
+            var tuple = stack.Pop();
+            
+            if (tuple.Node.left == null && tuple.Node.right == null)
+            {
+                sum += tuple.Value;
+                continue;
+            }
+
+            if (tuple.Node.right != null)
+                stack.Push((tuple.Node.right, (tuple.Value << 1) | tuple.Node.right.val));
+
+            if (tuple.Node.left != null)
+                stack.Push((tuple.Node.left, (tuple.Value << 1) | tuple.Node.left.val));
+        }
+
+        return sum;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,14 @@
-﻿using LeetCode._1461._Check_If_a_String_Contains_All_Binary_Codes_of_Size_K;
+﻿using LeetCode._1022._Sum_of_Root_To_Leaf_Binary_Numbers;
 
 var solution = new Solution();
 
-const string s = "11001";
-const int k = 2;
-var ans = solution.HasAllCodes(s, k);
+var ll = new TreeNode();
+var lr = new TreeNode(1);
+var rl = new TreeNode();
+var rr = new TreeNode(1);
+var l = new TreeNode(0, ll, lr);
+var r = new TreeNode(1, rl, rr);
+var root = new TreeNode(1, l, r);
+
+var ans = solution.SumRootToLeaf(root);
 Console.WriteLine(string.Join(" ", ans));
