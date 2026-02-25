8cce865d6019b770aa25d51d23f4a5df90dd0b85
W1ngshot <kirill.samsonov18@gmail.com>
2026-02-25 11:49:37 +0300

    1356 done

---
+++ 1356. Sort Integers by The Number of 1 Bits/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1356._Sort_Integers_by_The_Number_of_1_Bits;
+
+public class Solution
+{
+    public int[] SortByBits(int[] arr)
+    {
+        Array.Sort(arr, (el1, el2) =>
+        {
+            var bits1 = int.PopCount(el1);
+            var bits2 = int.PopCount(el2);
+            
+            return bits1 == bits2 ? el1 - el2 : bits1 - bits2;
+        });
+        
+        return arr;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,7 @@
-﻿using LeetCode._228._Summary_Ranges;
+﻿using LeetCode._1356._Sort_Integers_by_The_Number_of_1_Bits;
 
 var solution = new Solution();
 
-var nums = new[] { 0, 1, 2, 4, 5, 6, 8 };
-var ans = solution.SummaryRanges(nums);
+var nums = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+var ans = solution.SortByBits(nums);
 Console.WriteLine(string.Join(" ", ans));
