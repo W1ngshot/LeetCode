da8d03e3af597a5ce6185da3afd9ab655dcde7a9
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-03 13:45:18 +0300

    1545 done

---
+++ 1545. Find Kth Bit in Nth Binary String/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._1545._Find_Kth_Bit_in_Nth_Binary_String;
+
+public class Solution
+{
+    public char FindKthBit(int n, int k)
+    {
+        if (n == 1 || k % 4 == 1)
+            return '0';
+
+        if (k % 4 == 3)
+            return '1';
+
+        var middle = 1 << (n - 1);
+        var ans = '1';
+
+        while (middle != k)
+        {
+            ans = k > middle ? '0' : '1';
+            k %= middle;
+            middle /= 2;
+        }
+
+        return ans;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,13 +1,12 @@
-﻿using LeetCode._1536._Minimum_Swaps_to_Arrange_a_Binary_Grid;
+﻿using LeetCode._1545._Find_Kth_Bit_in_Nth_Binary_String;
 
 var solution = new Solution();
 
-var n = new[]
+var n = 3;
+var k = 7;
+var ans = new List<char>();
+for (var i = 1; i <= k; i++)
 {
-        new []{1,1,1,0},
-        new []{1,1,0,0},
-        new []{1,0,0,0},
-        new []{0,0,0,0},
-};
-var ans = solution.MinSwaps(n);
+    ans.Add(solution.FindKthBit(n, i));
+}
 Console.WriteLine(string.Join(" ", ans));
