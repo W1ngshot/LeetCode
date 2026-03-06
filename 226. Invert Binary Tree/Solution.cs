b7cbad07379d4a3f79ae659501f80eeb405c1116
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-06 20:19:35 +0300

    226 done

---
+++ 226. Invert Binary Tree/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode._226._Invert_Binary_Tree;
+
+//Definition for a binary tree node.
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
+
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root is null)
+            return root;
+
+        var queue = new Queue<TreeNode?>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (node is null)
+                continue;
+
+            (node.left, node.right) = (node.right, node.left);
+
+            queue.Enqueue(node.left);
+            queue.Enqueue(node.right);
+        }
+
+        return root;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,14 +1,14 @@
-﻿using LeetCode._101._Symmetric_Tree;
+﻿using LeetCode._226._Invert_Binary_Tree;
 
 var solution = new Solution();
 
-var ll = new TreeNode(3);
-var lr = new TreeNode(4);
+var ll = new TreeNode(1);
+var lr = new TreeNode(3);
 var l = new TreeNode(2, ll, lr);
-var rl = new TreeNode(4);
-var rr = new TreeNode(3);
-var r = new TreeNode(2, rl, rr);
-var root = new TreeNode(1, l, r);
+var rl = new TreeNode(6);
+var rr = new TreeNode(9);
+var r = new TreeNode(7, rl, rr);
+var root = new TreeNode(4, l, r);
 
-var ans = solution.IsSymmetric(root);
+var ans = solution.InvertTree(root);
 Console.WriteLine(string.Join(" ", ans));
