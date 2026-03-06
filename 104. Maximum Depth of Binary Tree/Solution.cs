bfc3f1bb8477f3a8525c69f5436e373a94e9d8f8
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-06 03:24:43 +0300

    104 done

---
+++ 104. Maximum Depth of Binary Tree/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode._104._Maximum_Depth_of_Binary_Tree;
+
+//Definition for a binary tree node.
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
+
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        return root is null ? 0 : GetDepth(root, 1);
+    }
+
+    private int GetDepth(TreeNode node, int depth)
+    {
+        var left = node.left is null ? depth : GetDepth(node.left, depth + 1);
+        var right = node.right is null ? depth : GetDepth(node.right, depth + 1);
+
+        return Math.Max(left, right);
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,12 @@
-﻿using LeetCode._1784._Check_if_Binary_String_Has_at_Most_One_Segment_of_Ones;
+﻿using LeetCode._104._Maximum_Depth_of_Binary_Tree;
 
 var solution = new Solution();
 
-var s = "110";
-var ans = solution.CheckOnesSegment(s);
+var l = new TreeNode(9);
+var rl = new TreeNode(15);
+var rr = new TreeNode(7);
+var r = new TreeNode(20, rl, rr);
+var root = new TreeNode(3, l, r);
+
+var ans = solution.MaxDepth(root);
 Console.WriteLine(string.Join(" ", ans));
