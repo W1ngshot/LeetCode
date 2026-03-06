4fa2da1545899553addc65e3ebc3ac58514bb207
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-06 03:50:38 +0300

    101 done

---
+++ 101. Symmetric Tree/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode._101._Symmetric_Tree;
+
+//Definition for a binary tree node.
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+}
+
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if (root.left is null && root.left == root.right)
+            return true;
+
+        var leftQueue = new Queue<TreeNode?>();
+        var rightQueue = new Queue<TreeNode?>();
+
+        leftQueue.Enqueue(root.left);
+        rightQueue.Enqueue(root.right);
+
+        while (leftQueue.Count > 0 && rightQueue.Count > 0)
+        {
+            var leftQueueNode = leftQueue.Dequeue();
+            var rightQueueNode = rightQueue.Dequeue();
+
+            if (leftQueueNode?.val != rightQueueNode?.val)
+                return false;
+
+            if (leftQueueNode is null)
+                continue;
+
+            leftQueue.Enqueue(leftQueueNode.left);
+            leftQueue.Enqueue(leftQueueNode.right);
+
+            rightQueue.Enqueue(rightQueueNode!.right);
+            rightQueue.Enqueue(rightQueueNode.left);
+        }
+
+        return leftQueue.Count == rightQueue.Count;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,14 @@
-﻿using LeetCode._104._Maximum_Depth_of_Binary_Tree;
+﻿using LeetCode._101._Symmetric_Tree;
 
 var solution = new Solution();
 
-var l = new TreeNode(9);
-var rl = new TreeNode(15);
-var rr = new TreeNode(7);
-var r = new TreeNode(20, rl, rr);
-var root = new TreeNode(3, l, r);
+var ll = new TreeNode(3);
+var lr = new TreeNode(4);
+var l = new TreeNode(2, ll, lr);
+var rl = new TreeNode(4);
+var rr = new TreeNode(3);
+var r = new TreeNode(2, rl, rr);
+var root = new TreeNode(1, l, r);
 
-var ans = solution.MaxDepth(root);
+var ans = solution.IsSymmetric(root);
 Console.WriteLine(string.Join(" ", ans));
