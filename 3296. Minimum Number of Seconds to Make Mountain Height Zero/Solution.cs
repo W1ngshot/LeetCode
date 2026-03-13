dadc82a72b2efe16bbc0b4c728044ebe5ec87d2e
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-13 12:38:13 +0300

    3296 done

---
+++ 3296. Minimum Number of Seconds to Make Mountain Height Zero/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode._3296._Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero;
+
+public class Solution
+{
+    public long MinNumberOfSeconds(int mountainHeight, int[] workerTimes)
+    {
+        var queue = new PriorityQueue<int, int>();
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < workerTimes.Length; i++)
+            queue.Enqueue(i, workerTimes[i]);
+
+        var max = 0;
+        
+        for (var i = 0; i < mountainHeight; i++)
+        {
+            var worker = queue.Dequeue();
+
+            if (!dict.TryAdd(worker, 1))
+                dict[worker]++;
+
+            var count = dict[worker];
+            if ((1 + count) * count * workerTimes[worker] / 2 > max)
+                max = (1 + count) * count * workerTimes[worker] / 2;
+            
+            queue.Enqueue(worker, (1 + 1 + count) * (1 + count) * workerTimes[worker] / 2);
+        }
+
+        return max;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,6 @@
-﻿using LeetCode._3600._Maximize_Spanning_Tree_Stability_with_Upgrades;
+﻿using LeetCode._3296._Minimum_Number_of_Seconds_to_Make_Mountain_Height_Zero;
 
 var solution = new Solution();
 
-var ans = solution.MaxStability(6, new int[][]
-{
-    [0,1,7,1],[1,2,3,0],[2,3,8,0],[3,4,6,0],[4,5,5,0]
-}, 1);
+var ans = solution.MinNumberOfSeconds(5, [1]);
 Console.WriteLine(string.Join(" ", ans));
