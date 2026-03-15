6a1f37cf7ad08d5fa9a4cdb03434d075621e8b37
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-15 19:48:48 +0300

    1622 done

---
+++ 1622. Fancy Sequence/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+
+namespace LeetCode._1622._Fancy_Sequence;
+
+public class Fancy
+{
+    private readonly List<int> _numbers = [];
+    private long _sum;
+    private long _mult = 1;
+    private const int Mod = 1_000_000_007;
+
+    public void Append(int val)
+    {
+        var n = val - _sum;
+        if (n < 0)
+            n += Mod;
+        var inverse = (long)BigInteger.ModPow(_mult, Mod - 2, Mod);
+        var num = (int)(n * inverse % Mod);
+        _numbers.Add(num);
+    }
+
+    public void AddAll(int inc)
+    {
+        _sum = (_sum + inc) % Mod;
+    }
+
+    public void MultAll(int m)
+    {
+        _sum = _sum * m % Mod;
+        _mult = _mult * m % Mod;
+    }
+
+    public int GetIndex(int idx)
+    {
+        if (idx >= _numbers.Count)
+            return -1;
+        return (int)(((_numbers[idx] * _mult % Mod) + _sum) % Mod);
+    }
+}
+
+/*
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy obj = new Fancy();
+ * obj.Append(val);
+ * obj.AddAll(inc);
+ * obj.MultAll(m);
+ * int param_4 = obj.GetIndex(idx);
+ */
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,19 @@
-﻿using LeetCode._1415._The_k_th_Lexicographical_String_of_All_Happy_Strings_of_Length_n;
+﻿using LeetCode._1622._Fancy_Sequence;
 
-var solution = new Solution();
+var fancy = new Fancy();
 
-var ans = solution.GetHappyString(3, 8);
-Console.WriteLine(string.Join(" ", ans));
+fancy.Append(2);   // fancy sequence: [2]
+fancy.AddAll(3);   // fancy sequence: [2+3] -> [5]
+fancy.Append(7);   // fancy sequence: [5, 7]
+fancy.MultAll(2);  // fancy sequence: [5*2, 7*2] -> [10, 14]
+var ans1 = fancy.GetIndex(0); // return 10
+Console.WriteLine(ans1);
+fancy.AddAll(3);   // fancy sequence: [10+3, 14+3] -> [13, 17]
+fancy.Append(10);  // fancy sequence: [13, 17, 10]
+fancy.MultAll(2);  // fancy sequence: [13*2, 17*2, 10*2] -> [26, 34, 20]
+var ans2 = fancy.GetIndex(0); // return 26
+Console.WriteLine(ans2);
+var ans3 = fancy.GetIndex(1); // return 34
+Console.WriteLine(ans3);
+var ans4 = fancy.GetIndex(2); // return 20
+Console.WriteLine(ans4);
