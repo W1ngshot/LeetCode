6a5382a7593062f6fde9400b200270021d71fc01
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-18 01:08:27 +0300

    1727 done

---
+++ 1727. Largest Submatrix With Rearrangements/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode._1727._Largest_Submatrix_With_Rearrangements;
+
+public class Solution
+{
+    public int LargestSubmatrix(int[][] matrix)
+    {
+        var ans = 0;
+
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            for (var j = 0; j < matrix[0].Length; j++)
+            {
+                if (i != 0 && matrix[i][j] != 0)
+                    matrix[i][j] = matrix[i - 1][j] + 1;
+            }
+
+            var row = matrix[i].ToArray();
+            Array.Sort(row, (a, b) => b - a);
+
+            for (var k = 0; k < row.Length; k++)
+            {
+                if (row[k] == 0)
+                    break;
+
+                ans = Math.Max(ans, (k + 1) * row[k]);
+            }
+        }
+
+        return ans;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,7 @@
-﻿using LeetCode._1878._Get_Biggest_Three_Rhombus_Sums_in_a_Grid;
+﻿using LeetCode._1727._Largest_Submatrix_With_Rearrangements;
 
 var solution = new Solution();
 
-int[][] grid = [[3, 4, 5, 1, 3], [3, 3, 4, 2, 3], [20, 30, 200, 40, 10], [1, 5, 5, 4, 1], [4, 3, 2, 2, 5]];
-var ans = solution.GetBiggestThree(grid);
+int[][] matrix = [[1,1,0],[1,0,1]];
+var ans = solution.LargestSubmatrix(matrix);
 Console.WriteLine(string.Join(' ', ans));
