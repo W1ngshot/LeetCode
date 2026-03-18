5e48aea53dd47e6d4ea241b662fed9754ecc460e
W1ngshot <kirill.samsonov18@gmail.com>
2026-03-18 23:53:44 +0300

    3070 done

---
+++ 3070. Count Submatrices with Top-Left Element and Sum Less Than k/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode._3070._Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k;
+
+public class Solution
+{
+    public int CountSubmatrices(int[][] grid, int k)
+    {
+        var result = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[0].Length; j++)
+            {
+                grid[i][j] += (i > 0 ? grid[i - 1][j] : 0)
+                              + (j > 0 ? grid[i][j - 1] : 0)
+                              - (i > 0 && j > 0 ? grid[i - 1][j - 1] : 0);
+
+                if (grid[i][j] <= k)
+                    result++;
+                else
+                    break;
+            }
+        }
+
+        return result;
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,7 @@
-﻿using LeetCode._1727._Largest_Submatrix_With_Rearrangements;
+﻿using LeetCode._3070._Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k;
 
 var solution = new Solution();
 
-int[][] matrix = [[1,1,0],[1,0,1]];
-var ans = solution.LargestSubmatrix(matrix);
+int[][] matrix = [[7,2,9],[1,5,0],[2,6,6]];
+var ans = solution.CountSubmatrices(matrix, 20);
 Console.WriteLine(string.Join(' ', ans));
